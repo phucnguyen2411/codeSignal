2b00a8ddcbc3803e60361da5ffdde86789c8b98d
Phuc .Ng <phucnguyen.2411@gmail.com>
2019-03-19 23:45:30 +0700

    #2: rotate the image

--- src/comm/phucng/codesignal/DataStructures/arrays.java
+++ src/comm/phucng/codesignal/DataStructures/arrays.java
@@ -32,4 +32,25 @@ public class arrays {
 		}
 	}
 
+	public static int[][] rotateImage(int[][] a) {
+		int temp;
+		int n= a.length;
+		for(int i = 0; i<n; i++){
+			for(int j = i; j<a[0].length; j++){
+				temp = a[i][j];
+				a[i][j] = a[j][i];
+				a[j][i] = temp;
+			}
+		}
+		for(int i =0 ; i<n; i++){
+			for(int j = 0; j<n/2; j++){
+				temp = a[i][j];
+				a[i][j] = a[i][n-1-j];
+				a[i][n-1-j] = temp;
+			}
+		}
+
+		return a;
+	}
+
 }
